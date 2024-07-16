b5e9e8239bb5c9c44eceb2a7bcbc7dc8d44c062d
ngnamquoc <nguyennamquoc.mar@gmail.com>
2024-07-16 08:40:31 -0400

    player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-
+import Player from "./components/Player"
 function App() {
   
 
   return (
-    <p>comming soon..</p>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X"/>
+          <Player name="Player 2" symbol="O"/>
+        </ol>
+        GAME BOARD
+
+      </div>
+    </main>
   )
 }
 
