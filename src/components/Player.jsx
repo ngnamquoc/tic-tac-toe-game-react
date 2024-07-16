19652079aaedac909b4a743450862cabf323c631
ngnamquoc <nguyennamquoc.mar@gmail.com>
2024-07-16 09:54:00 -0400

    edit and save username

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,33 @@
-export default function Player({name,symbol}){
+import { useState } from "react";
+export default function Player({initialName,symbol}){
+    const [userName, setName]=useState(initialName);
+    const [isEditing, setIsEditing]=useState(false);
+    function handleEditToggle(){
+        //update state instantly by arrow function
+        setIsEditing((editing)=>!editing);
+        // console.log("editing mode" + isEditing);
+    }
+    function handleChange(event){
+        //apply 2-way-binding concept
+        // console.log(event.target.value);
+        setName(event.target.value);
+    }
+    //default playerName & button title
+    let playerName=<span className="player-name">{userName}</span>
+    let buttonTitle="Edit";
+    //When on editing mode
+    if(isEditing){
+        playerName=<span className="player-name"><input onChange={handleChange} required defaultValue={name} type="text" placeholder="Type Name..."/></span>;
+        buttonTitle="Save";
+    }
+    
     return(
         <li>
-            <span>
-              <span className="player-name">{name}</span>
+            <span className="player">
+              {playerName}
               <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditToggle}>{buttonTitle}</button>
         </li>
 
     );
